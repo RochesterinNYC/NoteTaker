d3b190bab37997e07b018808887d21f2d2876209
James Wen <jrw2175@columbia.edu>
2014-05-08 15:46:45 -0400

    Initial commit

---
+++ NoteTaker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NoteTaker
+//
+//  Created by James Wen on 5/8/14.
+//  Copyright (c) 2014 Rochester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JRWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JRWAppDelegate class]));
+    }
+}
